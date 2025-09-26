5c0fb918c254d42f0b6afc396f8266439a61de0a
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-09-26 20:47:07 +0200

    fix(src): add logging

--- src/main/java/dev/weinmann/runnerz/Application.java
+++ src/main/java/dev/weinmann/runnerz/Application.java
@@ -1,23 +1,22 @@
 package dev.weinmann.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-//import WelcomeMessage;
 
 @SpringBootApplication
 public class Application {
 
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
-		//SpringApplication.run(Application.class, args);
-    //var welcomeMessage = new WelcomeMessage();
-    //System.out.println(welcomeMessage.getWelcomeMessage());
 
-    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 
-    WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
-    System.out.println(welcomeMessage.getWelcomeMessage());
-
-	}
+    log.info("Application started successfully!");
+    log.warn("Something changed");
+	
+  }
 
 }

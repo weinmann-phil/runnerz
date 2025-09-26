27bbaf562dbc42647affe8ffc04e70dafd467c0a
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-09-26 20:27:14 +0200

    fix(src): learn how to handle context

--- src/main/java/dev/weinmann/runnerz/Application.java
+++ src/main/java/dev/weinmann/runnerz/Application.java
@@ -2,15 +2,20 @@ package dev.weinmann.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import foo.bar.WelcomeMessage;
+import org.springframework.context.ConfigurableApplicationContext;
+//import WelcomeMessage;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		//SpringApplication.run(Application.class, args);
+    //var welcomeMessage = new WelcomeMessage();
+    //System.out.println(welcomeMessage.getWelcomeMessage());
 
-    var welcomeMessage = new WelcomeMessage();
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+    WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
     System.out.println(welcomeMessage.getWelcomeMessage());
 
 	}
--- src/main/java/dev/weinmann/runnerz/WelcomeMessage.java
+++ src/main/java/dev/weinmann/runnerz/WelcomeMessage.java
@@ -1,4 +1,4 @@
-package foo.bar;
+package dev.weinmann.runnerz;
 
 import org.springframework.stereotype.Component;
 

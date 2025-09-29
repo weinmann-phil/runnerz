2be6fa42b1523cd9bed3d76928c44b088700f090
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-09-29 21:40:44 +0200

    fix(src): finish basic CRUD operations

--- src/main/java/dev/weinmann/runnerz/run/RunController.java
+++ src/main/java/dev/weinmann/runnerz/run/RunController.java
@@ -1,12 +1,14 @@
 package dev.weinmann.runnerz.run;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -42,6 +44,18 @@ public class RunController {
   @PostMapping("")
   void create(@RequestBody Run run) {
     runRepository.create(run);
+  }
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PutMapping("/{id}")
+  void update(@RequestBody Run run, @PathVariable Integer id) {
+    runRepository.update(run,id);
+  }
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @DeleteMapping("/{id}")
+  void delete(@PathVariable Integer id) {
+    runRepository.delete(id);
   }
 
 }
